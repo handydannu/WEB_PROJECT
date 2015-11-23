a92b12c3111487171a98904429260e62fd6c6089
Handy Dannu Wijaya <handydannu@hotmail.com>
2015-11-23 08:17:54 +0700

    Graph MOSI
    Upload MOSI Graph

---
+++ MOSI - Pemodelan Simulasi/grafik.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <?php include 'includes/head.php'; ?>     	
+    </head> 
+<body> 
+<div id="mosi_header_wrapper">
+	<div id="mosi_header">
+    	<div id="site_title">PROGRAM PEMODELAN SIMULASI</div>
+        <p id="intro_text">PROGRAM PEMODELAN SIMULASI. Untuk menghitung soal resiprok</p>
+    </div>
+</div>
+<div id="mosi_main_wrapper">
+	<div id="mosi_main">
+		<div id="content"> 
+        <center>
+            <div id="beranda" class="section">
+              <img src = "grp.php" />
+            </div>
+        </center>   
+         </div> 
+    </div>
+</div>
+
+<div id="mosi_footer_wrapper">
+	<div id="mosi_footer">
+    	<p>Copyright © 2014 | <a href="http://moccadev.com">Handy D - MoccaDev</a> | Program Pemodelan Simulasi</p>
+    </div>
+</div>
+
+</body> 
+</html>
